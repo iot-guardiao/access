51c010c2378a7d7917e500132b0c6f4adb8426c5
Ellem Amorim <ellemalmeidaamorim@gmail.com>
2025-01-17 08:35:01 -0300

    modify to main

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,46 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-const char *ssid = "ellem";
+const char *ssid = "LITTORAL";
 const char *password = "12345678";
 
-const char *url = "http://httpbin.org/ip";
+const char *url = "http://192.168.5.182:8000/";
 
 // See available models on README.md or ESP32CameraPins.h
 ESP32QRCodeReader reader(CAMERA_MODEL_AI_THINKER);
 
-unsigned long previousMillis = 0;
-const long interval = 5000; 
-String text;
+String qrPayload = ""; // Armazena o conteúdo do QR code
+bool qrPayloadReady = false; // Flag para indicar se o QR code foi lido e está pronto para envio
+
+String httpGETRequest(const char *serverName)
+{
+  WiFiClient client;
+  HTTPClient http;
+
+  http.begin(client, serverName);
+
+  // Send HTTP GET request
+  int httpResponseCode = http.GET();
+
+  String payload = "--";
+
+  if (httpResponseCode > 0)
+  {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+  }
+  else
+  {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+
+  return payload;
+}
 
 void onQrCodeTask(void *pvParameters)
 {
@@ -25,48 +54,61 @@ void onQrCodeTask(void *pvParameters)
       Serial.println("Found QRCode");
       if (qrCodeData.valid)
       {
+        qrPayload = String((const char *)qrCodeData.payload);
+        qrPayloadReady = true; // Sinaliza que há um novo QR code pronto
         Serial.print("Payload: ");
-        Serial.println((const char *)qrCodeData.payload);
+        Serial.println(qrPayload);
       }
       else
       {
-        Serial.print("Invalid: ");
+        Serial.print("Invalid QR Code: ");
         Serial.println((const char *)qrCodeData.payload);
+        qrPayload = "";         // Limpa o payload inválido
+        qrPayloadReady = false; // Flag não pronta
       }
     }
     vTaskDelay(100 / portTICK_PERIOD_MS);
   }
 }
 
-String httpGETRequest(const char* serverName) {
-  WiFiClient client;
-  HTTPClient http;
-    
-  // Your Domain name with URL path or IP address with path
-  http.begin(client, serverName);
-  
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-  
-  String payload = "--"; 
-  
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
+void onHttpRequestTask(void *pvParameters)
+{
+  unsigned long previousMillis = 0;
+  const long interval = 5000;
 
-  return payload;
+  while (true)
+  {
+    unsigned long currentMillis = millis();
+
+    if (currentMillis - previousMillis >= interval && qrPayloadReady)
+    {
+      // Check WiFi connection status
+      if (WiFi.status() == WL_CONNECTED)
+      {
+        String requestUrl = String(url) + "?qr="  + "benjamin";
+        Serial.println("Send:" + requestUrl);
+        String response = httpGETRequest(requestUrl.c_str());
+        Serial.println("HTTP Response: " + response);
+
+        qrPayload = "";         // Limpa o payload após o envio
+        qrPayloadReady = false; // Reseta a flag após o envio
+      }
+      else
+      {
+        Serial.println("WiFi Disconnected");
+      }
+
+      previousMillis = currentMillis;
+    }
+
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
 }
 
 void setup()
 {
+  Serial.begin(115200);
+
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -77,30 +119,16 @@ void setup()
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-  Serial.begin(115200);
-  Serial.println();
 
   reader.setup();
   reader.beginOnCore(1);
+
+  // Create tasks
   xTaskCreate(onQrCodeTask, "onQrCode", 4 * 1024, NULL, 4, NULL);
+  xTaskCreate(onHttpRequestTask, "onHttpRequest", 4 * 1024, NULL, 4, NULL);
 }
 
 void loop()
 {
-  unsigned long currentMillis = millis();
-  
-  if(currentMillis - previousMillis >= interval) {
-     // Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED ){ 
-      text = httpGETRequest(url);
-
-      Serial.println("Retorno: " + text);
-      
-
-      previousMillis = currentMillis;
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-  }
+  // O loop está vazio porque todas as operações são realizadas nas tasks.
 }
